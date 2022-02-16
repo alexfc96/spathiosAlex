058c69218538a44062e075e7fc21a4dfb2bb5a12
Alex Fernandez <alexfernandez@beondev.com>
2022-02-16 11:44:26 +0100

    put method adding 'bookings' to spaces

--- database/db.sql
+++ database/db.sql
@@ -19,9 +19,16 @@ CREATE TABLE IF NOT EXISTS spaces(
 
 INSERT INTO spaces (listingName, pricePerHour, listingBusy) VALUES('Espacio 1', 50, '[{"startDateTime": "2022-02-15T08:00:00", "endDateTime": "2022-02-15T10:30:00", "status": "booked"}]');
 INSERT INTO spaces (listingName, pricePerHour, listingBusy) VALUES('Espacio 2', 125, '[{"startDateTime": "2022-02-18T11:00:00", "endDateTime": "2022-02-18T12:00:00", "status": "blocked"}]');
+INSERT INTO spaces (listingName, pricePerHour, listingBusy) VALUES('Espacio 3', 60, '[{"startDateTime": "2022-02-20T11:00:00", "endDateTime": "2022-02-20T12:00:00", "status": "booked"}]');
 
+UPDATE spaces SET listingBusy = listingBusy || '{"startDateTime": "2022-02-10T08:00:00", "endDateTime": "2022-02-10T11:00:00", "status": "blocked"}' ::jsonb WHERE listingID = 3;
 
 -- https://www.guru99.com/postgresql-array-functions.html
+
+
+--Tendremos que crear una tabla para los bookings.
+-- id de reserva, fecha de check in,
+-- fecha de check out, precio total de la reserva, y la nueva ocupación del listing
 
 --Modelo de datos solicitado:
 -- {
--- src/pages/api/spaces/[id].ts
+++ src/pages/api/spaces/[id].ts
@@ -19,20 +19,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             } catch (error: any) {
                 return res.status(500).json({ message: error.message })
             }
-        // case 'PUT':
-        //     try {
-        //         const { title, description } = body;
-        //         const text = "UPDATE spaces SET title = $1, description = $2 WHERE id = $3 RETURNING *";
-        //         const values = [title, description, query.id];
-        //         const result = await conn.query(text, values);
+        case 'PUT':
+            try {
+                const { listingBusy } = body;
+                
+                //tener en cuenta que he puesto las comillas a los valores manualmente
+                const text = `UPDATE spaces SET listingBusy = listingBusy || '{"startDateTime": "${listingBusy.startDateTime}", "endDateTime": "${listingBusy.endDateTime}", "status": "${listingBusy.status}"}' ::jsonb WHERE listingID = ${query.id} RETURNING *`;
+                const result = await conn.query(text);
     
-        //         if(result.rows.length === 0) 
-        //             return res.status(400).json({ message: "Task not found" })
+                if(result.rows.length === 0) 
+                    return res.status(400).json({ message: "Space not found" })
     
-        //         return res.json(result.rows[0]);
-        //     } catch (error: any) {
-        //         return res.status(500).json({ message: error.message })
-        //     }
+                return res.json(result.rows[0]);
+            } catch (error: any) {
+                return res.status(500).json({ message: error.message })
+            }
         default:
             return res.status(400).json('method not allowed');
     }
