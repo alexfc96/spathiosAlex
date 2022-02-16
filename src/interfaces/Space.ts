560ae5e786a263782819f0fd729c3cb68ac1f6f4
Alex Fernandez <alexfernandez@beondev.com>
2022-02-16 01:48:23 +0100

    improving space interface

--- src/interfaces/Space.ts
+++ src/interfaces/Space.ts
@@ -2,5 +2,11 @@ export interface Space {
     listingID?: string;
     listingName: string;
     pricePerHour: number;
-    listingBusy?: object;
+    listingBusy?: ListingBusy;
+}
+
+interface ListingBusy extends Space {
+    startDateTime: Date;
+    endDateTime: Date;
+    status: string;
 }
